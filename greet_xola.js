b058de87a1be6fa2290a346225455853316a2c5c
siphenkosi <salmansiphenkosi@gmail.com>
2018-07-23 16:37:26 +0200

    initial commit

---
+++ greet_xola.js
@@ -0,0 +1,17 @@
+//const chalk = require('chalk');
+//
+//const greet = require('./greet');
+//
+//const styledMessage = chalk.bgRed.black(greet('Xola'));
+//console.log(greet(styledMessage))
+
+var figlet = require('figlet');
+ 
+figlet('Hello Salman!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
